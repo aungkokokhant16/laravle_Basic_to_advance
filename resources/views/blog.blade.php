1197c507fa0c02589ae8675db5fa93aae3705891
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2022-08-10 22:38:14 +0630

    2.drawback-of-static-or-hardcoding-blog-website

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Blog</title>
+</head>
+
+<body>
+    <article>
+        <h1><a href="/blog">First Blog</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, sapiente. Dolorem libero officia
+            voluptatum, autem iste dolorum eius quidem, perferendis aut ullam odit ipsa. Debitis nihil consequatur quia
+            quos sunt.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita deserunt, quia obcaecati quo magni illum
+            non dolorem, minima omnis quidem nesciunt iure laborum odit ex, commodi atque nihil ab inventore?
+
+        </p>
+
+        <a href="/">go back</a>
+    </article>
+
+
+
+
+
+
+</body>
+
+</html>
