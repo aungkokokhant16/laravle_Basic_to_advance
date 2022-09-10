9d2ef5697ca4b6de6094ac508a34dcfce2fce0fa
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2022-09-10 10:30:49 +0630

    Ep-14-Collection-part-2(sorting)

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -12,21 +12,24 @@ class Blog{
     public $slug;
     public $intro;
     public $body;
+    public $date;
 
-    public function __construct($title,$slug,$intro,$body)
+    public function __construct($title,$slug,$intro,$body,$date)
     {
         $this->title=$title;
         $this->slug=$slug;
         $this->intro=$intro;
         $this->body=$body;
+        $this->date=$date;
     }
 
     public static function all(){
         return collect(File::files(resource_path("blogs")))
             ->map(function($file){
             $obj = YamlFrontMatter::parseFile($file);
-            return new Blog ($obj->title,$obj->slug,$obj->intro,$obj->body());
-        });
+            return new Blog ($obj->title,$obj->slug,$obj->intro,$obj->body(),$obj->date);
+        })->sortByDesc('date');
+
 
         // return array_map( function($file){
         //     $obj = YamlFrontMatter::parseFile($file);
