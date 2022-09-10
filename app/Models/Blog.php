86282cb015fd8d5425165dfdd83ffbd10efb0896
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2022-09-10 15:26:52 +0630

    Ep-19-find-vs-findOrFail (two tricky methods)

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
@@ -68,5 +69,13 @@ class Blog{
     public static function find($slug){
         $blogs = static::all();
         return $blogs->firstWhere('slug',$slug);
+    }
+
+    public static function findOrFail($slug){
+        $blog = static::find($slug);
+        if(!$blog){
+            throw new ModelNotFoundException();
+        }
+        return $blog;
     }
 }
