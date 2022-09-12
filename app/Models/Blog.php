a5a69d85bf12058fd1736d4a8ee799a3adabcce5
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2022-09-12 23:55:32 +0630

    Ep-28-belongsTo Relationship

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Blog extends Model
 {
     use HasFactory;
     // protected $fillable = ['title','body','intro'];
     protected $guarded = []; //ထည့်ခြင်တာအကုန်ထည့်လို့ရတယ်။
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -8,6 +8,9 @@
 {{-- <div class={{$loop->odd ? 'bg-yellow' : ''}}> --}}
     <h1><a href="blogs/{{$blog->slug}}">{{$blog->title}}</a></h1>
     <div>
+        <p>
+            <a href="">{{$blog->category->name}}</a>
+        </p>
     <p> published at - {{$blog->created_at->diffforHumans()}}</p>
     <p>{{$blog->title}}</p>
 </div>
