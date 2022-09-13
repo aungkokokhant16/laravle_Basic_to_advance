36f6670c2b958b0cfbaa4bac60ad3e64abd2b37c
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2022-09-13 12:03:44 +0630

    Ep-31-what-the-heck-is-db-seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Blog;
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();//truncate()ဆိုတာ မ run ခင်  data table ထဲမှာရှိတဲ့ data အားလုံးကို အရင်ဖျက်
+        Category::truncate();
+        Blog::truncate();
+        User::factory()->create();
+
+        $frontend=Category::create([
+            "name"=>"frontend",
+            "slug"=>"frontend"
+        ]);
+
+        $backend=Category::create([
+            "name"=>"backend",
+            "slug"=>"backend"
+        ]);
+
+        Blog::create([
+            "title"=>"Frontend",
+            "intro"=>"This is Frontend Intro",
+            "slug"=>"frontend-post",
+            "body"=>"This is body text",
+            "category_id"=>$frontend->id
+        ]);
+
+        Blog::create([
+            "title"=>"Backend",
+            "intro"=>"This is Backend Intro",
+            "slug"=>"backend-post",
+            "body"=>"This is body text",
+            "category_id"=>$backend->id
+        ]);
     }
 }
