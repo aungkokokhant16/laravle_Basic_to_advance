4f420f65990603928dfb13b5ee05056865c113cf
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2022-09-14 12:54:44 +0630

    Ep-36-Design-integration-with-components-extract

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,12 +1,12 @@
-
+{{--
 <x-layouts>
 
     <x-slot name="title">
         All Blogs
     </x-slot>
-    @foreach ($blogs as $blog)
+    @foreach ($blogs as $blog) --}}
 {{-- <div class={{$loop->odd ? 'bg-yellow' : ''}}> --}}
-    <h1><a href="blogs/{{$blog->slug}}">{{$blog->title}}</a></h1>
+    {{-- <h1><a href="blogs/{{$blog->slug}}">{{$blog->title}}</a></h1>
     <h4>Author - <a href="/users/{{$blog->author->username}}">{{$blog->author->name}}</a> </h4>
     <div>
         <p>
@@ -14,10 +14,16 @@
         </p>
     <p> published at - {{$blog->created_at->diffforHumans()}}</p>
     <p>{{$blog->title}}</p>
-</div>
+</div> --}}
 {{-- </div> --}}
-@endforeach
+{{-- @endforeach
+</x-layouts> --}}
+<x-layouts>
+    <x-hero/>
+    <!-- blogs section -->
+    <x-blog-section />
+    {{-- Subscribe --}}
+    <x-subscribe />
 </x-layouts>
-
 
 
--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,7 +1,11 @@
-<nav>
-
-    <a href="">Main</a>
-    <a href="">About</a>
-    <a href="">Content</a>
-
-</nav>
+<!-- navbar -->
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="./index.html">Creative Coder</a>
+      <div class="d-flex">
+        <a href="#home" class="nav-link">Home</a>
+        <a href="#blogs" class="nav-link">Blogs</a>
+        <a href="#subscribe" class="nav-link">Subscribe</a>
+      </div>
+    </div>
+  </nav>
