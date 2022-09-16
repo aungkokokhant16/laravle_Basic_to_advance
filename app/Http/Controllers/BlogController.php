f8e8b7ee99943c641f24013f2f3714f13a7b26ac
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2022-09-16 15:38:59 +0630

    Ep-43-introducing to controller and simple query refactoring

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function index(){
+
+    return view('blogs',[
+        'blogs'=>$this->getBlogs(),//Blog::all ကိုသုံးလို့မရ with နဲ့ သုံးရင် get query ကိုဘဲ သုံးလို့ရမယ် with ကို lazy loading or eager load လို့ခေါ်
+        'categories'=>Category::all()
+
+    ]);
+    }
+
+    public function show(Blog $blog){//{blog:slug} slug နဲ့ရှာစေခြင်တာ
+    return view('blog',[
+        'blog'=>$blog,//wildcard nameနဲ့တူရမယ်
+        'randomBlogs'=>Blog::inRandomOrder()->take(3)->get()
+    ]);
+    }
+
+    protected function getBlogs(){
+        $blogs=Blog::latest();
+        if(request('search')){
+        $blogs=$blogs->where('title','LIKE','%'.request('search').'%')
+                    ->orWhere('body','LIKE','%'.request('search').'%');
+    }
+    return $blogs->get();
+    }
+}
--- resources/views/components/blog-section.blade.php
+++ resources/views/components/blog-section.blade.php
@@ -40,11 +40,17 @@
       </div>
     </form>
     <div class="row">
-      @foreach ($blogs as $blog)
+
+
+      @forelse ($blogs as $blog)
       <div class="col-md-4 mb-4">
         <x-blog-card :blog="$blog"/>
         </div>
-      @endforeach
+      @empty
+          <p class="text-danger">No Blogs Found.</p>
+      @endforelse
+
+
 
 
     </div>
