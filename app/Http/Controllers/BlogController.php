2557a5ae90ffb9596e30407cc39c5cb482df75ee
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2022-09-21 20:10:11 +0630

    Ep-51-simple-paginate-trick

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -12,6 +12,7 @@ class BlogController extends Controller
         'blogs'=>Blog::latest()
             ->filter(request(['search','category','username']))
             ->paginate(6)//Blog::all ကိုသုံးလို့မရ with နဲ့ သုံးရင် get query ကိုဘဲ သုံးလို့ရမယ် with ကို lazy loading or eager load လို့ခေါ်
+            //->simplePaginate(6)//Next and Prev ဘဲပါ
             ->withQueryString()//frontend ကို‌ေ၇ွးပီး paginate ရွေးရင် ပျောက်မသွားအောင်သုံး
 
     ]);
