6dc5ac5a8ae8d301b93a824d8f388c3559fd0a3f
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2022-09-22 17:23:30 +0630

    Ep-53-Registration-Design-integration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
@@ -22,4 +23,4 @@ Route::get('/', [BlogController::class,'index']);
 Route::get('/blogs/{blog:slug}',[BlogController::class,'show']);
 
 
-
+Route::get('/register', [AuthController::class,'create']);
