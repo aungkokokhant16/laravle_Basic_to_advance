2786c30c6b83594d3e328e94bebaac37ab166c77
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2022-09-22 12:36:41 +0630

    Ep-52-Laravel-controller-and-view-naming-conventions

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,7 +8,7 @@ use App\Models\Blog;
 class BlogController extends Controller
 {
     public function index(){
-        return view('blogs',[
+        return view('blogs.index',[
         'blogs'=>Blog::latest()
             ->filter(request(['search','category','username']))
             ->paginate(6)//Blog::all ကိုသုံးလို့မရ with နဲ့ သုံးရင် get query ကိုဘဲ သုံးလို့ရမယ် with ကို lazy loading or eager load လို့ခေါ်
@@ -19,7 +19,7 @@ class BlogController extends Controller
     }
 
     public function show(Blog $blog){//{blog:slug} slug နဲ့ရှာစေခြင်တာ
-    return view('blog',[
+    return view('blog.show',[
         'blog'=>$blog,//wildcard nameနဲ့တူရမယ်
         'randomBlogs'=>Blog::inRandomOrder()->take(3)->get()
     ]);
