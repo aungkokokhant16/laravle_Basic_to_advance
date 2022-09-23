b5a569bd43ab684b952d398f06173a7afcf60d39
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2022-09-23 10:17:29 +0630

    Ep-54-Post-method-and-CSRF-concept

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -4,22 +4,23 @@
             <div class="col-md-5 mx-auto">
                 <h3 class="text-primary text-center my-3">Register Form</h3>
                 <div class="card p-4 my-3 shadow-sm">
-                    <form>
+                    <form method="POST">
+                        @csrf
                         <div class="form-group mb-3">
                             <label for="exampleInputEmail1">Name</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                           </div>
                           <div class="form-group mb-3">
                             <label for="exampleInputEmail1">UserName</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <input type="text" name="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                           </div>
                         <div class="form-group mb-3">
                           <label for="exampleInputEmail1">Email address</label>
-                          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                          <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                         </div>
                         <div class="form-group mb-3">
                           <label for="exampleInputPassword1">Password</label>
-                          <input type="password" class="form-control" id="exampleInputPassword1">
+                          <input type="password" name="password" class="form-control" id="exampleInputPassword1">
                         </div>
 
                         <button type="submit" class="btn btn-primary">Submit</button>
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ Route::get('/blogs/{blog:slug}',[BlogController::class,'show']);
 
 
 Route::get('/register', [AuthController::class,'create']);
+
+Route::post('/register', [AuthController::class,'store']);
